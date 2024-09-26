19869ef9855310f1094b05b6f7ff496dac6a26a0
irsyadakmal99 <irsyadakmal95@gmail.com>
2024-09-26 21:18:44 +0700

    commit1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const userName = prompt("Hi, Name");
+console.log(userName);
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const tanggalLahir = document.getElementById("tanggal lahir");
+const jenisKelamin = document.getElementById("jenis kelamin");
